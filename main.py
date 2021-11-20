0cf543be9ef0aac45c31ba2a8a01edb410848147
Biffyclyro <joao-senna1@hotmail.com>
2021-11-20 17:20:29 -0300

    trabalhando em deletar a palavra

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from pynput import keyboard
+import clipboard
 
 word = ""
+
+
+def paste_hotkey(text):
+    key_controller = keyboard.Controller()
+    print('chegou aqui')
+    #key_controller.type(text)
 
 def on_press(key):
     global word
@@ -12,10 +19,15 @@ def on_press(key):
         word = word[:-1]
     elif hasattr(key, 'char'):
         word += key.char
-        print('a',word)
+        #print('a',word)
 
     if word == "teste":
-        print('rolou')   
+        paste_hotkey('escreve isso')
+        #clipboard.copy('era pra funcionar assim')
+        #with key_controller.press(keyboard.Key.ctrl.value):
+        #    key_controller.press('v')
+        #    key_controller.release('v')
+
 
 listener = keyboard.Listener(on_press=on_press)
 listener.start()
