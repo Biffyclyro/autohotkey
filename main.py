996085b1f6fe4ecfc344d8cd638554f47e09b329
Biffyclyro <joao-senna1@hotmail.com>
2021-11-24 17:16:41 -0300

    removendo um atalho de forma permanente

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ def paste_hotkey(text):
     global auto_press
     auto_press = True
     key_controller = keyboard.Controller()
-    #with key_controller.pressed(keyboard.Key.ctrl):
-    #    key_controller.tap(keyboard.Key.left)
     
     with key_controller.pressed(keyboard.Key.ctrl):
         with key_controller.pressed(keyboard.Key.shift):
@@ -23,15 +21,13 @@ def paste_hotkey(text):
 def on_press(key):
     global word
     global auto_press
-    #print(key)
+    #print(word)
     
     if not auto_press:
 
         if key == keyboard.Key.space:
             word = ""
 
-        elif key == keyboard.Key.backspace:
-            word = word[:-1]
         elif hasattr(key, 'char'):
             word += key.char
 
